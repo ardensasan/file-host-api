07817f413ced3b36378590b62ce6567dffa32b90
Arden Johannes Sasan <ajsasan@dnamicro.com>
2022-04-20 14:19:39 +0800

    save to env file mongodb url

--- src/mongodb.ts
+++ src/mongodb.ts
@@ -1,11 +1,11 @@
 import { MongoClient } from "mongodb";
-const uri =
-  "mongodb+srv://template:template@template.jzp0h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+import dontenv from 'dotenv'
+dontenv.config()
 let promise: any = null;
 export const dbConnect = () => {
   if (promise) return promise;
   promise = new Promise((resolve, reject) => {
-    const conn = new MongoClient(uri);
+    const conn = new MongoClient(process.env.MONGO_DB_URI || '');
     conn
       .connect()
       .then(() => {
